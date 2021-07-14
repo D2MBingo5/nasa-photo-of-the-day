efe68a3240bbee1b1612f455d228581bac0babeb
Leu Family <romanianfobjr@gmail.com>
2021-07-14 15:35:24 -0500

    axios installed, App.js properly receiving axios and constants/index.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./App.css";
+import axios from 'axios'
+import { BASE_URL, API_KEY } from './constants/index'
 
 function App() {
   return (
     <div className="App">
+      <header className='App-header'>
+        NASA Astronomy Picture of the Day!
+      </header>
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
