d8e797c7e8c10a54c66de5f080230d4003864066
Leu Family <romanianfobjr@gmail.com>
2021-07-14 16:24:46 -0500

    apod information properly displaying on DOM

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from 'axios'
 import { BASE_URL, API_KEY } from './constants/index'
+import ReactPlayer from "react-player"
 
 function App() {
+  const [apod, setApod] = useState('')
+
+  useEffect(() => {
+    // api call
+    console.log('in useEffect')
+    axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+      .then(res => {
+        console.log(res.data)
+        setApod(res.data)
+        
+      })
+      .catch(err => console.log(err))
+  }, [])
+
+console.log(apod.date)
+
   return (
     <div className="App">
       <header className='App-header'>
         NASA Astronomy Picture of the Day!
       </header>
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <div>
+        <p>{apod.date} - {apod.title}</p>
+        <div className='App-video'>
+         <ReactPlayer url={apod.url} /> 
+        </div> {/* Problem: This will only work on days where there is a video as the Picture of the Day. This can be made more dynamic by having a component in a separate file generate HTML depending on the content type (apod.media_type) */}
+        <p>{apod.explanation}</p>
+      </div>      
       <footer className='App-footer'>        
         <a className='App-link' href='https://apod.nasa.gov/apod/astropix.html'>Click here to go to the NASA website's APOD page!</a>
       </footer>
