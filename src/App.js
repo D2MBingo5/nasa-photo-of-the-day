a1d8e6db91b732f14dc300827c91aed250cf43ca
Leu Family <romanianfobjr@gmail.com>
2021-07-15 15:06:33 -0500

    App.js now properly displays either a video or image depending on apod.media_type

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   useEffect(() => {
     // api call
     console.log('in useEffect')
-    axios.get(`${BASE_URL}?api_key=${API_KEY}${SPEC_DATE}`)
+    axios.get(`${BASE_URL}?api_key=${API_KEY}`) // For current day, remove ${SPEC_DATE} from the api call string
       .then(res => {
         console.log(res.data)
         setApod(res.data)
@@ -19,20 +19,20 @@ function App() {
       .catch(err => console.log(err))
   }, [])
 
-console.log(apod.date)
+// console.log(apod.media_type)
 
   return (
     <div className="App">
       <header className='App-header'>
         NASA Astronomy Picture of the Day!
       </header>
-      <div>
-        <p>{apod.date} - {apod.title}</p>
-        <div className='App-insert'>
-         <ReactPlayer url={apod.url} /> 
-         <img src={apod.url} alt='NASA APOD'></img> {/* TEMPORARY SOLUTION */}
-        </div> {/* Problem: This will only work on days where there is a video as the Picture of the Day. This can be made more dynamic by having a component in a separate file generate HTML depending on the content type (apod.media_type) */}
-        <p>{apod.explanation}</p>
+      <div className='content-container'>
+        <p className='p-area'>{apod.date} - {apod.title}</p>
+        <div>{
+          (apod.media_type === 'video' ? <ReactPlayer url={apod.url} /> : <img src={apod.url} alt='NASA APOD'></img>)
+        } {/* This div and ternary statement say that  if the APOD of the day 'media_type' is a video, render a ReactPlayer with the apod.url, if it's not a video, render an img. I don't forsee any media types besides videos and images, so this may work on any day. Time will tell */}
+        </div>
+        <p className='p-area'>{apod.explanation}</p>
       </div>      
       <footer className='App-footer'>        
         <a className='App-link' href='https://apod.nasa.gov/apod/astropix.html'>Click here to go to the NASA website's APOD page!</a>
